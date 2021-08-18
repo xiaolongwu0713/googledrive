1f9e91b5496d81b6ab9995f4ba9397ee58a5d27f
long <xiaolongwu0713@gmail.com>
2021-08-18 08:07:51 +0800

    separate grasp and gesture to two projects

--- examples/braindecode_examples/plot_bcic_iv_2a_moabb_trial.py
+++ examples/braindecode_examples/plot_bcic_iv_2a_moabb_trial.py
@@ -1,5 +1,39 @@
+"""
+Trialwise Decoding on BCIC IV 2a Dataset
+========================================
+
+This tutorial shows you how to train and test deep learning models with
+Braindecode in a classical EEG setting: you have trials of data with
+labels (e.g., Right Hand, Left Hand, etc.).
+
+"""
+
+######################################################################
+# Loading and preprocessing the dataset
+# -------------------------------------
+#
+
+
+######################################################################
+# Loading
+# ~~~~~~~
+#
+
+
+######################################################################
+# First, we load the data. In this tutorial, we use the functionality of
+# braindecode to load datasets through
+# `MOABB <https://github.com/NeuroTechX/moabb>`__ to load the BCI
+# Competition IV 2a data.
+#
+# .. note::
+#    To load your own datasets either via mne or from
+#    preprocessed X/y numpy arrays, see `MNE Dataset
+#    Tutorial <./plot_mne_dataset_example.html>`__ and `Numpy Dataset
+#    Tutorial <./plot_custom_dataset_example.html>`__.
+#
+
 from braindecode.datasets.moabb import MOABBDataset
-import mne
 
 subject_id = 3
 dataset = MOABBDataset(dataset_name="BNCI2014001", subject_ids=[subject_id])
@@ -26,8 +60,8 @@ dataset = MOABBDataset(dataset_name="BNCI2014001", subject_ids=[subject_id])
 #    `torchvision <https://pytorch.org/docs/stable/torchvision/index.html>`__.
 #
 
-from braindecode.datautil.preprocess import exponential_moving_standardize
-from braindecode.datautil.preprocess import MNEPreproc, NumpyPreproc, preprocess
+from braindecode.datautil.preprocess import (
+    exponential_moving_standardize, preprocess, Preprocessor)
 
 low_cut_hz = 4.  # low cut frequency for filtering
 high_cut_hz = 38.  # high cut frequency for filtering
@@ -36,19 +70,30 @@ factor_new = 1e-3
 init_block_size = 1000
 
 preprocessors = [
-    # keep only EEG sensors
-    MNEPreproc(fn='pick_types', eeg=True, meg=False, stim=False),
-    # convert from volt to microvolt, directly modifying the numpy array
-    NumpyPreproc(fn=lambda x: x * 1e6),
-    # bandpass filter
-    MNEPreproc(fn='filter', l_freq=low_cut_hz, h_freq=high_cut_hz),
-    # exponential moving standardization
-    NumpyPreproc(fn=exponential_moving_standardize, factor_new=factor_new,
-        init_block_size=init_block_size)
+    Preprocessor('pick_types', eeg=True, meg=False, stim=False),  # Keep EEG sensors
+    Preprocessor(lambda x: x * 1e6),  # Convert from V to uV
+    Preprocessor('filter', l_freq=low_cut_hz, h_freq=high_cut_hz),  # Bandpass filter
+    Preprocessor(exponential_moving_standardize,  # Exponential moving standardization
+                 factor_new=factor_new, init_block_size=init_block_size)
 ]
 
 # Transform the data
 preprocess(dataset, preprocessors)
+
+
+######################################################################
+# Cut Compute Windows
+# ~~~~~~~~~~~~~~~~~~~
+#
+
+
+######################################################################
+# Now we cut out compute windows, the inputs for the deep networks during
+# training. In the case of trialwise decoding, we just have to decide if
+# we want to cut out some part before and/or after the trial. For this
+# dataset, in our work, it often was beneficial to also cut out 500 ms
+# before the trial.
+#
 
 import numpy as np
 from braindecode.datautil.windowers import create_windows_from_events
@@ -69,6 +114,19 @@ windows_dataset = create_windows_from_events(
     preload=True,
 )
 
+
+######################################################################
+# Split dataset into train and valid
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+
+######################################################################
+# We can easily split the dataset using additional info stored in the
+# description attribute, in this case ``session`` column. We select
+# ``session_T`` for training and ``session_E`` for validation.
+#
+
 splitted = windows_dataset.split('session')
 train_set = splitted['session_T']
 valid_set = splitted['session_E']
@@ -79,9 +137,21 @@ valid_set = splitted['session_E']
 # ------------
 #
 
+
+######################################################################
+# Now we create the deep learning model! Braindecode comes with some
+# predefined convolutional neural network architectures for raw
+# time-domain EEG. Here, we use the shallow ConvNet model from `Deep
+# learning with convolutional neural networks for EEG decoding and
+# visualization <https://arxiv.org/abs/1703.05051>`__. These models are
+# pure `PyTorch <https://pytorch.org>`__ deep learning models, therefore
+# to use your own model, it just has to be a normal PyTorch
+# `nn.Module <https://pytorch.org/docs/stable/nn.html#torch.nn.Module>`__.
+#
+
 import torch
 from braindecode.util import set_random_seeds
-from braindecode.models import ShallowFBCSPNet,Deep4Net
+from braindecode.models import ShallowFBCSPNet
 
 cuda = torch.cuda.is_available()  # check if GPU is available, if True chooses to use it
 device = 'cuda' if cuda else 'cpu'
@@ -91,12 +161,12 @@ seed = 20200220  # random seed to make results reproducible
 # Set random seed to be able to reproduce results
 set_random_seeds(seed=seed, cuda=cuda)
 
-n_classes=4
+n_classes = 4
 # Extract number of chans and time steps from dataset
 n_chans = train_set[0][0].shape[0]
 input_window_samples = train_set[0][0].shape[1]
 
-model = Deep4Net(
+model = ShallowFBCSPNet(
     n_chans,
     n_classes,
     input_window_samples=input_window_samples,
@@ -106,6 +176,29 @@ model = Deep4Net(
 # Send model to GPU
 if cuda:
     model.cuda()
+
+
+
+######################################################################
+# Training
+# --------
+#
+
+
+######################################################################
+# Now we train the network! EEGClassifier is a Braindecode object
+# responsible for managing the training of neural networks. It inherits
+# from skorch.NeuralNetClassifier, so the training logic is the same as in
+# `Skorch <https://skorch.readthedocs.io/en/stable/>`__.
+#
+
+
+######################################################################
+#    **Note**: In this tutorial, we use some default parameters that we
+#    have found to work well for motor decoding, however we strongly
+#    encourage you to perform your own hyperparameter optimization using
+#    cross validation on your training data.
+#
 
 from skorch.callbacks import LRScheduler
 from skorch.helper import predefined_split
@@ -154,6 +247,7 @@ clf.fit(train_set, y=None, epochs=n_epochs)
 import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
 import pandas as pd
+
 # Extract loss and accuracy values for plotting from history object
 results_columns = ['train_loss', 'valid_loss', 'train_accuracy', 'valid_accuracy']
 df = pd.DataFrame(clf.history[:, results_columns], columns=results_columns,

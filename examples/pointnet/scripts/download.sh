e1fd30d5044ffa6b7f9f687f2c6b295486624186
long <xiaolongwu0713@gmail.com>
2021-08-27 10:00:13 +0800

    added pointnet example

---
+++ examples/pointnet/scripts/download.sh
@@ -0,0 +1,9 @@
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
+echo $SCRIPT
+echo $SCRIPTPATH
+#cd $SCRIPTPATH/..
+#wget https://shapenet.cs.stanford.edu/ericyi/shapenetcore_partanno_segmentation_benchmark_v0.zip --no-check-certificate
+#unzip shapenetcore_partanno_segmentation_benchmark_v0.zip
+#rm shapenetcore_partanno_segmentation_benchmark_v0.zip
+#cd -

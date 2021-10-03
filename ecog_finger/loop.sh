e30a713ad60515abecae00a8b48ca61657a91664
long <xiaolongwu0713@gmail.com>
2021-10-03 16:39:49 +0800

    haha

--- ecog_finger/loop.sh
+++ ecog_finger/loop.sh
@@ -1,12 +1,14 @@
 #/bin/bash
 cwd=`pwd`
-echo $cwd
-if [[ $cwd == C*  ]];then
+if [[ $HOSTNAME == "workstation"  ]];then
 	source /cygdrive/c/Users/wuxiaolong/venv/Scripts/activate
-elif [[ $cwd == "/usr"  ]];then
-	source /usr/local/venv/gesture/bin/activate
+	echo "workstation"
 fi
-for sid in 1 2 3 4 5 6 7 8 9
-do
-	python main_base.py $sid
-done
+if [[ $HOSTNAME == "longsMac"  ]];then
+	source /usr/local/venv/gesture/bin/activate
+	echo "longsMac"
+fi
+#for sid in 1 2 3 4 5 6 7 8 9
+#do
+#	python main_base.py $sid
+#done

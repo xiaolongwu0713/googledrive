bd1f56e81dee509534193fc317c38b662a75879e
long <xiaolongwu0713@gmail.com>
2021-10-04 09:51:42 +0800

    some adjust for Scott

--- ecog_finger/config.py
+++ ecog_finger/config.py
@@ -1,16 +1,19 @@
 import os, re
-
+import socket
 
 sids=[1,2,3,4,5,6,7,8,9]
 
+if socket.gethostname() == 'ScottsMachine': # change this to your $HOSTNAME
+    data_dir = '/your/data/dir/'  # running on Scott's machine
+
 location=os.getcwd()
-if re.compile('/Users/long/').match(location):
-    data_dir='/Users/long/Documents/data/ecog/' # preprocessed data
+if re.compile('/Users/long/').match(location): # long's machine
+    data_dir='/Users/long/Documents/data/ecog/'
     tmp_data_dir='/Users/long/Documents/data/gesture/'
-    root_dir = '/Users/long/BCI/python_scripts/googleDrive/'  # this is project root
-elif re.compile('/content/drive').match(location):
-    data_dir='/content/drive/MyDrive/data/ecog/' # googleDrive
-    root_dir='/content/drive/MyDrive/' # googleDrive
-elif re.compile('C:').match(location):
-    data_dir='C:/Users/wuxiaolong/Desktop/BCI/data/' # googleDrive
-    root_dir='C:/Users/wuxiaolong/Desktop/BCI/googledrive/' # googleDrive
+    root_dir = '/Users/long/BCI/python_scripts/googleDrive/'
+elif re.compile('/content/drive').match(location):  # googleDrive
+    data_dir='/content/drive/MyDrive/data/ecog/'
+    root_dir='/content/drive/MyDrive/'
+elif re.compile('C:').match(location): # windows workstation
+    data_dir='C:/Users/wuxiaolong/Desktop/BCI/data/'
+    root_dir='C:/Users/wuxiaolong/Desktop/BCI/googledrive/'

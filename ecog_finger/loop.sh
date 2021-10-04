fe3e44ddcb49c174a9457993b2cfda8c2b12d115
xiaolongwu0713 <xiaolongwu0713@gmail.com>
2021-10-04 03:07:51 +0100

    push from windows, added dos2unix.bat

--- ecog_finger/loop.sh
+++ ecog_finger/loop.sh
@@ -4,10 +4,16 @@ if [[ $HOSTNAME == "workstation"  ]];then
 	source /cygdrive/c/Users/wuxiaolong/venv/Scripts/activate
 	echo "workstation"
 fi
+
+
 if [[ $HOSTNAME == "longsMac"  ]];then
 	source /usr/local/venv/gesture/bin/activate
 	echo "longsMac"
 fi
+
+
+
+
 #for sid in 1 2 3 4 5 6 7 8 9
 #do
 #	python main_base.py $sid

3b564c7c1b7db7f2404bdfbbee11ee2eb719d0d7
long <xiaolongwu0713@gmail.com>
2021-11-07 20:04:02 +0800

    tf analysis for all subjects

---
+++ gesture/preprocess/dos2unix.bat
@@ -0,0 +1,1 @@
+C:\cygwin64\bin\dos2unix.exe loop_tf_all_channel.sh
---
+++ gesture/preprocess/loop_tf_all_channel.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# usage: ./loop_main_all.sh wind stride
+if [[ $HOSTNAME == "workstation"  ]];then
+	source /cygdrive/c/Users/wuxiaolong/venv/Scripts/activate
+	inputfile="H:/Long/data/gesture/preprocessing/Info.txt"
+	echo "workstation"
+fi
+
+
+if [[ $HOSTNAME == "longsMac"  ]];then
+	source /usr/local/venv/gesture/bin/activate
+	inputfile="/Users/long/Documents/data/gesture/info/Info.txt"
+	echo "longsMac"
+fi
+
+
+#declare -a sids
+sids=()
+while IFS= read -r line
+do
+ sid=${line%,*}
+ sids+=("$sid")
+ #echo $sid
+ #echo ${sids[@]}
+done < "$inputfile"
+#echo ${sids[@]}
+
+activ_channel='all'
+for sid in ${sids[@]}
+do
+  echo "Start sid: $sid"
+    python tf_all_channel.py $sid
+  #done
+done
+
